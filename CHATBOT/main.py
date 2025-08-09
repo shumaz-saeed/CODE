990a9385f7c6757e22dbd9b72391889e97ffdbaa
NOBODY <147626403+shumaz-saeed@users.noreply.github.com>
2025-08-09 22:14:51 +0500

    Update main.py

--- CHATBOT/main.py
+++ CHATBOT/main.py
@@ -1,9 +1,24 @@
+
 import streamlit as st
-import langchain_helper as lch
+from langchain_helper import get_response 
 
+st.title("LangChain Agent with Streamlit")
+st.write("This is a simple interface to interact with your LangChain agent.")
 
-st.title("weather chatbot")
-st.sidebar.header("select a city 🌆")
-st.sidebar.selectbox("city", ['lahore', 'karachi', 'islamabad', 'peshawar', 'quetta'], key="city")
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
-st.markdown(f'-----------------------------------------------\n  {lch.get_response("city")}\n-----------------------------------------------')
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = get_response(prompt)
+            st.markdown(response)
+    st.session_state.messages.append({"role": "assistant", "content": response})
